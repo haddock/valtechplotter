320bdab2d34f8cdda5f45c40fd4fde270a2c6860
Jon Eldeklint <jon@eldeklint.se>
2014-06-18 19:30:27 +0200

    Lägger till rubyfil för att prata med arduinon

---
+++ RubyMain.rb
@@ -0,0 +1,23 @@
+#simplest ruby program to read from arduino serial, 
+#using the SerialPort gem
+#(http://rubygems.org/gems/serialport)
+
+require "serialport"
+
+#params for serial port
+port_str = "/dev/tty.usbmodem1421"  #may be different for you
+baud_rate = 9600
+data_bits = 8
+stop_bits = 1
+parity = SerialPort::NONE
+
+sp = SerialPort.new(port_str, baud_rate, data_bits, stop_bits, parity)
+
+while true do
+	puts "Enter command to arduino"
+	input = gets.chomp
+	sp.write(input)
+	sleep 5
+end
+
+sp.close
